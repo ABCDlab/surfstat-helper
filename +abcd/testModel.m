9684d69abe915dc20139e78f791b83cf880b9665
nf <abcdlab-nf@users.noreply.github.com>
2015-03-12 15:55:38 -0400

    testModel: big update; map arguments to function variables; handle optional arguments properly; refactor variable names into mixedCase; region (AAL) labels still not supported but function works otherwise

--- +abcd/testModel.m
+++ +abcd/testModel.m
@@ -1,4 +1,4 @@
-function [contrast_results] = testModel(varargin)
+function [contrastResults] = testModel(varargin)
 % Generates peak tables, statistics and figures for a given model specified
 % by the following parameters (See Nick's CT script for how to set it up).
 %
@@ -20,7 +20,7 @@ function [contrast_results] = testModel(varargin)
 %   'colormap': default spectral
 %   'colorbarLimits': default actual min/max of data
 %   'clusterFormingThreshold': default 0.001
-%   'aal78': TODO
+%   'regionLabels': TODO
 
 isTerm = @(x) isa(x, 'term');
 isContrastSet = @(x) isstruct(x);  % TODO: could be more strict by requiring all of x's fields to match strcmp(class(xfield),'term');
@@ -33,11 +33,11 @@ p.addRequired('Y', @ismatrix);
 p.addRequired('Model', isTerm);
 p.addRequired('contrasts', isContrastSet);
 p.addRequired('avsurf', isSurface);
-p.addParamValue('avsurfPlot', struct(), isSurface);
+p.addParamValue('avsurfPlot', [], isSurface);
 p.addParamValue('mask', [], @islogical); % TODO: after parsing input, assert that dimensions of mask are correct for Y
 p.addParamValue('captionNotes', struct(), @isstruct);
 p.addParamValue('dfAdjust', 0, isIntegerNumeric);
-% p.addParamValue('aal78', .... % TODO: best to provide all AAL data in a single struct variable
+p.addParamValue('regionLabels', [], @isstruct);
 p.addParamValue('colormap', spectral, isColormap);
 p.addParamValue('colorbarLimits', [], @isvector); % could be more strict
 p.addParamValue('clusterFormingThreshold', 0.001, @isnumeric);
@@ -49,24 +49,32 @@ Y = p.Results.Y;
 Model = p.Results.Model;
 contrasts = p.Results.contrasts;
 avsurf = p.Results.avsurf;
-plot_notes = p.Results.captionNotes;
-df_adjust = p.Results.dfAdjust;
+avsurfPlot = p.Results.avsurfPlot;
+captionNotes = p.Results.captionNotes;
+dfAdjust = p.Results.dfAdjust;
 mask = p.Results.mask;
+imageColormap = p.Results.colormap;
+fixedColorbarLimits = p.Results.colorbarLimits;
 clusterFormingThreshold = p.Results.clusterFormingThreshold;
+regionLabels = p.Results.regionLabels;
 
 
 % handle defaults
 if (isempty(mask))
     mask = true(1,size(Y,2));
 end
+if (isempty(avsurfPlot))
+    avsurfPlot = p.Results.avsurf;
+end
+
+assert(isempty(regionLabels), 'Sorry, region labels are not supported yet! But they will be soon.');
 
 
 plotEffectSize = false;
 
-contrast_results = [];
+contrastResults = [];
 
 % MODEL
-figure;
 modelValues = double(Model);
 if size(modelValues,2) > 1
     modelParameterNames = char(Model);
@@ -76,6 +84,7 @@ if size(modelValues,2) > 1
 else
     hist(modelValues)
 end
+set(gcf, 'Name','cor(mdl)', 'NumberTitle', 'off');
 
 %disp('Cross-correlation of parameters in model:');
 %term(corrcoef(double(Model)), char(Model))
@@ -95,7 +104,7 @@ for s = char(Model)
     modelString = [modelString s{1}];
 end
 
-notes=''; for f=fields(plot_notes)'; notes=[notes plot_notes.(char(f)) ' ']; end
+notes=''; for f=fields(captionNotes)'; notes=[notes captionNotes.(char(f)) ' ']; end
 
 % plots will generally be captioned as such:
 % line 1) contrast term / peak npk thresholds / N DF
@@ -104,7 +113,7 @@ notes=''; for f=fields(plot_notes)'; notes=[notes plot_notes.(char(f)) ' ']; end
 
 % CONTRASTS
 f = fields(contrasts);
-contrast_results = [];
+contrastResults = [];
 for c = 1:numel(f);
 
     contrastname = f{c};
@@ -113,10 +122,10 @@ for c = 1:numel(f);
     labels = []; labels.name = contrastname; labels.short = lower(labels.name);
 
     slm = slm_main;
-    if df_adjust > 0
+    if dfAdjust > 0
         % e.g. if we factored out a covariate from Y before running model,
         % should adjust DF accordingly
-        slm.df = slm.df - df_adjust;
+        slm.df = slm.df - dfAdjust;
     end
 
     % RUN CONTRAST
@@ -131,22 +140,24 @@ for c = 1:numel(f);
     if numel(peak) > 0
         peak.label_name = [];
         term( peak ) + term( SurfStatInd2Coord( peak.vertid, avsurf )', {'x','y','z'})
-        for i = 1:size(peak.vertid,1)
-            label = aal_labels(peak.vertid(i));
-            if label > 0 && label <= 78;
-                label_name = aal_78_ids{2}{label};
-                label_name_full = aal_78_ids{3}{label};
-            else
-                label_name = '(not defined)';
-                label_name_full = '(not defined)';
+        if ~isempty(regionLabels)
+            for i = 1:size(peak.vertid,1)
+                label = regionLabels(peak.vertid(i));
+                if label > 0 && label <= 78;
+                    label_name = aal_78_ids{2}{label};
+                    label_name_full = aal_78_ids{3}{label};
+                else
+                    label_name = '(not defined)';
+                    label_name_full = '(not defined)';
+                end
+                if i <= 10
+                    fprintf('peak #%d at vertid %5d is in AAL region: %s (%s)\n', i, peak.vertid(i), label_name, label_name_full);
+                end
+                peak.label_name{i} = label_name;
+                peak.label_name_full{i} = label_name_full;
             end
-            if i <= 10
-                fprintf('peak #%d at vertid %5d is in AAL region: %s (%s)\n', i, peak.vertid(i), label_name, label_name_full);
-            end
-            peak.label_name{i} = label_name;
-            peak.label_name_full{i} = label_name_full;
+            fprintf('\n');
         end
-        fprintf('\n');
     else
         peak
         peak.label_name = [];
@@ -159,36 +170,36 @@ for c = 1:numel(f);
     n_note = sprintf(' [N=%i,DF=%i]', size(Model,1), slm.df);
 
     % PLOT T MAP
-    max_abs_tval = max([max(slm.t.*mask) abs(min(slm.t.*mask))]);
-    caption_text = sprintf('T: %s, pk=%.2f, npk=%.2f (0.05r=%.2f, 0.001u=%.2f) %s\n%s\n%s', labels.name, max(slm.t.*mask), min(slm.t.*mask), tthresh, tthreshUC001, n_note, modelString, notes);
-    contrast_results.(contrastname).figT = figure; SurfStatView( slm.t.*mask, avsurf_view );
-    colormap(image_colormap);
-    dafs=get(gcf,'defaultaxesfontsize'); set(gcf,'defaultaxesfontsize',12); suptitle(caption_text); set(gcf,'defaultaxesfontsize',dafs);
+    maxAbsoluteTValue = max([max(slm.t.*mask) abs(min(slm.t.*mask))]);
+    captionText = sprintf('T: %s, pk=%.2f, npk=%.2f (0.05r=%.2f, 0.001u=%.2f) %s\n%s\n%s', labels.name, max(slm.t.*mask), min(slm.t.*mask), tthresh, tthreshUC001, n_note, modelString, notes);
+    contrastResults.(contrastname).figT = figure; SurfStatView( slm.t.*mask, avsurfPlot );
+    colormap(imageColormap);
+    dafs=get(gcf,'defaultaxesfontsize'); set(gcf,'defaultaxesfontsize',12); suptitle(captionText); set(gcf,'defaultaxesfontsize',dafs);
     set(gcf,'Name',['T.' labels.short]);
     set(gcf, 'NumberTitle', 'off');
-    if numel(fixed_colorbar_limits) > 0
-        SurfStatColLim(fixed_colorbar_limits);
+    if numel(fixedColorbarLimits) > 0
+        SurfStatColLim(fixedColorbarLimits);
     else
-        SurfStatColLim([-1*max_abs_tval max_abs_tval]);
+        SurfStatColLim([-1*maxAbsoluteTValue maxAbsoluteTValue]);
     end
 
     % PLOT EFFECT SIZE
     if (plotEffectSize)
         figure; SurfStatView( slm.ef.*mask, avsurfinfl, ' ' );
-        caption_text = sprintf('Effect size (coef) for %s, pk=%.2f, npk=%.2f %s\n%s\n%s', labels.name, max(slm.ef.*mask), min(slm.ef.*mask), n_note, modelString, notes);
-        dafs=get(gcf,'defaultaxesfontsize'); set(gcf,'defaultaxesfontsize',9); suptitle(caption_text); set(gcf,'defaultaxesfontsize',dafs);
+        captionText = sprintf('Effect size (coef) for %s, pk=%.2f, npk=%.2f %s\n%s\n%s', labels.name, max(slm.ef.*mask), min(slm.ef.*mask), n_note, modelString, notes);
+        dafs=get(gcf,'defaultaxesfontsize'); set(gcf,'defaultaxesfontsize',9); suptitle(captionText); set(gcf,'defaultaxesfontsize',dafs);
         set(gcf,'Name',['ef.' labels.short]);
         set(gcf, 'NumberTitle', 'off');
     end
 
     % PLOT P MAP
-    contrast_results.(contrastname).figP = [];
+    contrastResults.(contrastname).figP = [];
     if isfield(peak, 'P')
-        min_pval = min([pval.P(boolean(mask)) pval.C(boolean(mask))])
-        if min_pval < 0.05
-            caption_text = sprintf('P: %s, pk=%.2f clThr=%s %s\n%s\n%s', labels.name, min_pval, num2str(clusterFormingThreshold), n_note, modelString, notes);
-            contrast_results.(contrastname).figP = figure; SurfStatView( pval, avsurf_view, ' ' );
-            dafs=get(gcf,'defaultaxesfontsize'); set(gcf,'defaultaxesfontsize',9); suptitle(caption_text); set(gcf,'defaultaxesfontsize',dafs);
+        minPValue = min([pval.P(boolean(mask)) pval.C(boolean(mask))])
+        if minPValue < 0.05
+            captionText = sprintf('P: %s, pk=%.2f clThr=%s %s\n%s\n%s', labels.name, minPValue, num2str(clusterFormingThreshold), n_note, modelString, notes);
+            contrastResults.(contrastname).figP = figure; SurfStatView( pval, avsurfPlot, ' ' );
+            dafs=get(gcf,'defaultaxesfontsize'); set(gcf,'defaultaxesfontsize',9); suptitle(captionText); set(gcf,'defaultaxesfontsize',dafs);
             set(gcf,'Name', ['P.' labels.short]);
             set(gcf, 'NumberTitle', 'off');
         end
@@ -196,25 +207,25 @@ for c = 1:numel(f);
 
     % PLOT Q MAP
     qval = SurfStatQ( slm, boolean(mask) );
-    min_qval = min(qval.Q(boolean(mask)))
-    contrast_results.(contrastname).figQ = [];
-    if min_qval < 0.05
-        caption_text = sprintf('Q: %s, pk=%.2f %s\n%s\n%s', labels.name, min_qval, n_note, modelString, notes);
-        contrast_results.(contrastname).figQ = figure; SurfStatView( qval, avsurf_view, ' ');
-        dafs=get(gcf,'defaultaxesfontsize'); set(gcf,'defaultaxesfontsize',9); suptitle(caption_text); set(gcf,'defaultaxesfontsize',dafs);
+    minQValue = min(qval.Q(boolean(mask)))
+    contrastResults.(contrastname).figQ = [];
+    if minQValue < 0.05
+        captionText = sprintf('Q: %s, pk=%.2f %s\n%s\n%s', labels.name, minQValue, n_note, modelString, notes);
+        contrastResults.(contrastname).figQ = figure; SurfStatView( qval, avsurfPlot, ' ');
+        dafs=get(gcf,'defaultaxesfontsize'); set(gcf,'defaultaxesfontsize',9); suptitle(captionText); set(gcf,'defaultaxesfontsize',dafs);
         set(gcf,'Name', ['Q.' labels.short]);
         set(gcf, 'NumberTitle', 'off');
     end
 
-    contrast_results.(contrastname).slm = slm;
-    contrast_results.(contrastname).peak = peak;
-    contrast_results.(contrastname).pval = pval;
-    contrast_results.(contrastname).clus = clus;
-    contrast_results.(contrastname).clusid = clusid;
-    contrast_results.(contrastname).qval = qval;
-    contrast_results.(contrastname).resels = resels;
-    contrast_results.(contrastname).tthresh_rft = tthresh;
-    contrast_results.(contrastname).tthresh_uc001 = tthreshUC001;
+    contrastResults.(contrastname).slm = slm;
+    contrastResults.(contrastname).peak = peak;
+    contrastResults.(contrastname).pval = pval;
+    contrastResults.(contrastname).clus = clus;
+    contrastResults.(contrastname).clusid = clusid;
+    contrastResults.(contrastname).qval = qval;
+    contrastResults.(contrastname).resels = resels;
+    contrastResults.(contrastname).tthresh_rft = tthresh;
+    contrastResults.(contrastname).tthresh_uc001 = tthreshUC001;
 end
 
 fprintf('Cluster forming threshold = %s\n', num2str(clusterFormingThreshold));
@@ -225,17 +236,17 @@ for c = 1:numel(f);
     nResult.pkRft = 0;
     nResult.clRft = 0;
     nResult.pkFdr = 0;
-    if isfield(contrast_results.(contrastname).peak,'vertid')
-        nResult.pkUc = size(contrast_results.(contrastname).peak.vertid,1);
+    if isfield(contrastResults.(contrastname).peak,'vertid')
+        nResult.pkUc = size(contrastResults.(contrastname).peak.vertid,1);
     end
-    if isfield(contrast_results.(contrastname).peak,'P')
-        nResult.pkRft = sum(contrast_results.(contrastname).peak.P<0.05);
+    if isfield(contrastResults.(contrastname).peak,'P')
+        nResult.pkRft = sum(contrastResults.(contrastname).peak.P<0.05);
     end
-    if isfield(contrast_results.(contrastname).clus,'P')
-        nResult.clRft = sum(contrast_results.(contrastname).clus.P<0.05);
+    if isfield(contrastResults.(contrastname).clus,'P')
+        nResult.clRft = sum(contrastResults.(contrastname).clus.P<0.05);
     end
-    if isfield(contrast_results.(contrastname).qval,'Q')
-        nResult.pkFdr = sum(contrast_results.(contrastname).qval.Q<0.05);
+    if isfield(contrastResults.(contrastname).qval,'Q')
+        nResult.pkFdr = sum(contrastResults.(contrastname).qval.Q<0.05);
     end
     fprintf('Contrast %-7s has %3i uc peaks, %3i RFT peaks, %3i RFT clus, %4i FDR peaks\n', contrastname, nResult.pkUc, nResult.pkRft, nResult.clRft, nResult.pkFdr);
 end
