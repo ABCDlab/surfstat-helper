c2ee4bf71528b8412c295eab7ac1f370cecd185f
nf <abcdlab-nf@users.noreply.github.com>
2015-03-23 17:39:10 -0400

    buildNumber: Bump to 1.0

--- +abcd/buildNumber.m
+++ +abcd/buildNumber.m
@@ -1,14 +1,17 @@
 function n = buildNumber()
     % Current build number for abcd package
     %
-    % Build numbers are decimals. The part before the decimal is the
+    % Build numbers are decimal values. The part before the decimal is the
     % 'major' build number, and the part after the decimal is the 'minor'
-    % build number.  
+    % build number.
     %
-    % Updates that increase the 'major' build number will require calling
-    % scripts to be changed for some functions. 
-    % 
+    % Updates that increase the 'major' build number involve changes that
+    % will require scripts using these functions to be checked and
+    % possibly updated (e.g. changes to existing function arguments,
+    % defaults, behavior or output).
+    %
     % See the requireBuildNumber function.
-    n = 0.1;
+
+    n = 1.0;
 
 end
